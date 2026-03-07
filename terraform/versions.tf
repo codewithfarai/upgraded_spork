89bde98d089eb9c808fa37e02fe2bac5aa5bca43
codewithfarai <faraiwande@gmail.com>
2026-03-07 12:14:18 +0000

    chore: addition of .env to .gitignore

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,8 +4,21 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.60"
+      version = "~> 1.57"
     }
   }
+  backend "s3" {
+    bucket = "tf-state-bucket"
+    key    = "terraform.tfstate"
+    endpoints = {
+      s3 = "https://fsn1.your-objectstorage.com"
+    }
 
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    use_path_style              = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    region                      = "eu-central"
+  }
 }
